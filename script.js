b5c7a6eb68e3f9eae8f17652cc31b4cffe8a1e15
Juan <juan1034diaz@yahoo.com>
2020-09-18 13:57:40 -0700

    Grid is now able to be reset and sized dynamically

--- script.js
+++ script.js
@@ -1,22 +1,21 @@
 let gridSize = 16;
+const container = document.querySelector("#mainContainer");
 
 function createGrid(size){
     //loop size times to create rows
     //loop size times per row to create columns
     //append to DOM
-    const container = document.querySelector("#mainContainer");
     let count = 0;
     for (let i = 0; i < size * size; i++) {
         container.setAttribute("style", "grid-template-columns: " + "auto ".repeat(size));
         let newSquare = document.createElement("div");
-        newSquare.setAttribute("id", "gridSquare" + String(count));
+        newSquare.setAttribute("id", "gridSquare" + String(count++));
         newSquare.classList.toggle("gridSquare");
-        newSquare.textContent = String(count++);
         container.appendChild(newSquare);
     }
 }
 
-createGrid(16);
+createGrid(gridSize);
 let gridSquares = document.querySelectorAll('.gridSquare');
   gridSquares.forEach(gridSquare => gridSquare.addEventListener('mouseover', changeColor)); //each key needs its own event listener
 
@@ -24,3 +23,17 @@ function changeColor(e){
     console.log(e);
     e.srcElement.classList.add("hovered");
 }
+
+function resetCanvas(){
+    while(container.childElementCount > 0){
+        console.log(container.childElementCount);
+        container.lastChild.remove();
+    }
+    gridSize = prompt("Enter a size for your new Canvas", "16");
+    createGrid(gridSize);
+    let gridSquares = document.querySelectorAll('.gridSquare');
+    gridSquares.forEach(gridSquare => gridSquare.addEventListener('mouseover', changeColor)); //each key needs its own event listener
+}
+
+let resetButton = document.querySelector('#resetButton');
+resetButton.addEventListener("click", resetCanvas);
