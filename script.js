3236eec0929771e563a252f9fc38096b31195a81
Juan <juan1034diaz@yahoo.com>
2020-09-18 13:23:13 -0700

    Color now stays on after a grid is hovered

--- script.js
+++ script.js
@@ -9,9 +9,18 @@ function createGrid(size){
     for (let i = 0; i < size * size; i++) {
         container.setAttribute("style", "grid-template-columns: " + "auto ".repeat(size));
         let newSquare = document.createElement("div");
+        newSquare.setAttribute("id", "gridSquare" + String(count));
         newSquare.classList.toggle("gridSquare");
         newSquare.textContent = String(count++);
         container.appendChild(newSquare);
     }
 }
+
 createGrid(16);
+let gridSquares = document.querySelectorAll('.gridSquare');
+  gridSquares.forEach(gridSquare => gridSquare.addEventListener('mouseover', changeColor)); //each key needs its own event listener
+
+function changeColor(e){
+    console.log(e);
+    e.srcElement.classList.add("hovered");
+}
