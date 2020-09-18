5bd382127124b9f231612c67ec453ac28d2c4d40
Juan <juan1034diaz@yahoo.com>
2020-09-18 12:39:46 -0700

    Now creates grid and changes color on hover

--- script.js
+++ script.js
@@ -1,2 +1,17 @@
 let gridSize = 16;
 
+function createGrid(size){
+    //loop size times to create rows
+    //loop size times per row to create columns
+    //append to DOM
+    const container = document.querySelector("#mainContainer");
+    let count = 0;
+    for (let i = 0; i < size * size; i++) {
+        container.setAttribute("style", "grid-template-columns: " + "auto ".repeat(size));
+        let newSquare = document.createElement("div");
+        newSquare.classList.toggle("gridSquare");
+        newSquare.textContent = String(count++);
+        container.appendChild(newSquare);
+    }
+}
+createGrid(16);
